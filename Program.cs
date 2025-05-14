6d268c1842e84c64dcf64f71a77e0f689eb61f5c
Akile Karatekin <akilekaratekin33@gmail.com>
2025-05-14 13:48:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+namespace Emlakci.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            KiralikEv ke = new KiralikEv(5000, 600, 4, "kiralık ev", 2, 100);
+            KiralikEv ke1 = new KiralikEv(4000, 500, 4, "kiralık ev", 3, 300);
+            KiralikEv ke2 = new KiralikEv(6000, 700, 4, "kiralık ev", 5, 500);
+
+            Console.WriteLine(ke.ToString());
+
+            var se = new SatilikEv(600, 4, "satılık ev", 3, 100);
+            var se1 = new SatilikEv(700, 5, "satılık ev", 4, 200);
+            var se2 = new SatilikEv(800, 6, "satılık ev", 5, 300);
+
+        }
+    }
+}
+
